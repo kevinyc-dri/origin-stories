de1421cc12cbb031b6ac05f4df937b5942b815ed
Kevin Choi <kevinyoungchoi@gmail.com>
2021-08-26 15:21:50 -0400

    added header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Stack, Heading } from '@chakra-ui/react';
+import { Stack, VStack, Image } from '@chakra-ui/react';
 
 export default function Header() {
   return (
     <div>
       <Stack as="nav" direction={["row"]} spacing={8} justifyContent="space-between">
-        <Heading justifyContent="left">Raid Guild</Heading>
-        <div>Manifesto</div>
-        <div>Services</div>
-        <div>Portfolio</div>
-        <div>Join</div>
-        <div>Hire</div>
+        <Image src="Rectangle.png" alt="Raid Guild" />
+          <div>Manifesto</div>
+          <div>Services</div>
+          <div>Portfolio</div>
+          <div>Join</div>
+          <div>Hire</div>
       </Stack>
+      <VStack>
+        <Image src="01_header.jpg" alt="Header Image"  />
+      </VStack>
     </div>
   )
 }
