579897f8fa40d04d5de48faf8c8f499ead08fccd
Kevin Choi <kevinyoungchoi@gmail.com>
2021-08-26 16:52:12 -0400

    added cards

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,43 @@
 import React from 'react'
-import { Stack, HStack, Link, Image } from '@chakra-ui/react';
+import { Stack, HStack, Link, Image, Box } from '@chakra-ui/react';
 
 export default function Header() {
   return (
-    <div>
-      <Stack as="nav" mt="41" ml="20" mr="30"  direction={["row"]} spacing={8} justifyContent="space-between">
+    <>
+      <HStack as="nav" mt="41" ml="20" spacing={8} justifyContent="space-between">
         <Image src="Rectangle.png" alt="Raid Guild" />
         <Link>Manifesto</Link>
         <Link>Services</Link>
         <Link>Portfolio</Link>
         <Link>Join</Link>
         <Link>Hire</Link>
-      </Stack>
+      </HStack>
       <Stack>
-        <Image src="01_header.jpg" alt="Header Image" m="30" ml="20" mr="20" />
+        <Image src="01_header.jpg" alt="Header Image" />
       </Stack>
-      <Stack mt="118.11">
+      <Stack mt="118.11" mb="40">
         <h1>SUBHEAD EXPLAINING THE PROJECT LORUM IPSUM ETSI DOLOR</h1>
       </Stack>
-      <HStack>
+      <HStack color='#FFFFFF'>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ridiculus facilisis diam at donec feugiat. Et elementum tristique risus, ut arcu. Nibh scelerisque turpis scelerisque ultrices non. Duis sem arcu bibendum pellentesque quis tincidunt eget. Porttitor eget vulputate ultricies neque. Non at mauris pharetra, ut. Ipsum commodo varius aliquet quis magna tristique in justo feugiat. Maecenas ultricies ullamcorper facilisis lacus bibendum. Auctor eu feugiat est ornare nullam libero lacus adipiscing. Euismod et leo nam elementum eros mauris egestas.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ridiculus facilisis diam at donec feugiat. Et elementum tristique risus, ut arcu. Nibh scelerisque turpis scelerisque ultrices non. Duis sem arcu bibendum pellentesque quis tincidunt eget. Porttitor eget vulputate ultricies neque. Non at mauris pharetra, ut. Ipsum commodo varius aliquet quis magna tristique in justo feugiat. Maecenas ultricies ullamcorper facilisis lacus bibendum. Auctor eu feugiat est ornare nullam libero lacus adipiscing. Euismod et leo nam elementum eros mauris egestas.</p>
       </HStack>
-    </div>
+
+      <HStack justifyContent="space-between">
+        <Image boxSize="355px" src="01.png" alt="01" />
+        <Image boxSize="355px" src="02.png" alt="02" />
+        <Image boxSize="355px" src="03.png" alt="03" />
+      </HStack>
+        <HStack justifyContent="space-between">
+        <Image boxSize="355px" src="04.png" alt="04" />
+        <Image boxSize="355px" src="05.png" alt="05" />
+        <Image boxSize="355px" src="06.png" alt="06" />
+      </HStack>
+        <HStack justifyContent="space-between">
+        <Image boxSize="355px" src="07.png" alt="07" />
+        <Image boxSize="355px" src="08.png" alt="08" />
+        <Image boxSize="355px" src="09.png" alt="09" />
+      </HStack>
+    </>
   )
 }
